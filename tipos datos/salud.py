196d02d128b9eab3af5f843d5120c1c6bade2e7f
ireneSc4 <iresancab2@alu.edu.gva.es>
2024-10-25 09:03:10 +0200

    saludos

---
+++ tipos datos/salud.py
@@ -0,0 +1,17 @@
+quieres_saludar = 'S'  
+NUM_MAX_SALUDOS=4
+num_saludos=0
+while quieres_saludar=='S':
+    print('Hola que tal!')	
+    num_saludo +=1
+    if num_saludo == NUM_MAX_SALUDOS:
+         print('Has alcanzado el nº máximo de salidos')
+         break
+    quieres_saludar=input('¿Quiere otro saludo? [S/N])')
+    while quieres_saludar not in ('S','N'):
+         quieres_saludar =input('solo se aceptan los caracteres \'S\' O \'N\'.¿Quiere otro saludo? [S/N]')
+        
+    
+print('que tenga un buen dia')
+    
+   
