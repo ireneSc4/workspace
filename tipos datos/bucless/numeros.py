702c0beab37a28095d890028afb74d0354c3355b
irenitaSC <irenesanchezc06@gmail.com>
2024-11-25 16:52:48 +0100

    bucles

---
+++ tipos datos/bucless/numeros.py
@@ -0,0 +1,9 @@
+valor_minimo=int(input('Escribe un valor minimo: '))
+valor_maximo=int(input('Escribe un valor maximo: '))
+
+for i in range({valor_minimo},{valor_maximo}):
+    if i % 2 == 0:
+        print('los numeros pares son {i}')
+
+
+
---
+++ tipos datos/bucless/rectangulo.py
@@ -0,0 +1,6 @@
+#vamos a dibujar un rectangulo
+altura=int(input('Cual es la altura: '))
+anchura=int(input('Cual es la anchura: '))
+
+for i in range (altura):
+    print('*'*anchura)
